1036de283ebcbce31ce6027749e14f8875e1231d
martijnschuman <martijnschuman@hotmail.com>
2024-01-30 11:42:08 +0100

    Initial commit

---
+++ SecurityWorkshop/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecurityWorkshop.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+    }
+}
